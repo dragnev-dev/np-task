ce417dc8fa97d9894893e281240cbc5f704f5ad8
Dragomir Dragnev <60980007+drago333@users.noreply.github.com>
2023-03-28 16:11:52 +0300

    Read csv values and do a bidirectional lookup

--- src/server/main.rs
+++ src/server/main.rs
@@ -3,8 +3,46 @@ use std::time;
 use std ::net::{TcpListener,TcpStream};
 use std::io::{Read,Write};
 use std::thread;
+use csv::Reader;
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::rc::Rc;
+use std::hash::Hash;
+use std::ops::Deref;
 
 fn main() -> io::Result<()>{
+    println!("10");
+    let mut zips_serialized =
+        Reader::from_path("assets/places_postcodes.csv")?;
+    println!("16");
+
+    zips_serialized.set_headers(csv::StringRecord::from(vec!["zip", "place", "municipality", "area"]));
+    // let mut zips: Vec<ZipEntry> = Vec::new();
+    let mut zipsTowns = BidiMap::new();
+
+    for result in zips_serialized.deserialize() {
+        // We must tell Serde what type we want to deserialize into.
+        let record: ZipEntry = result?;
+        // zips.push(record);
+        println!("{}, {}", record.zip, record.place);
+
+        zipsTowns.put(record.zip, record.place);
+    }
+    println!("zips loaded...");
+    println!("{:?}", zipsTowns.get(&1000).unwrap());
+    println!("{:?}", zipsTowns.get_reverse(&"София".to_string()).unwrap());
+
+    let dirALookup = zipsTowns.get(&65535);
+    let dirBLookup = zipsTowns.get_reverse(&"Чичово".to_string());
+
+    if dirALookup == None && dirBLookup == None {
+        println!("¯\\_(ツ)_/¯")
+    } else {
+        if dirALookup != None { println!("{}", dirALookup.unwrap()) }
+        if dirBLookup != None { println!("{}", dirBLookup.unwrap()) }
+    }
+
+
     // Enable port 7878 binding
     let receiver_listener = TcpListener::bind("127.0.0.1:7878").expect("Failed and bind with the sender");
     // Getting a handle of the underlying thread.
@@ -28,6 +66,47 @@ fn main() -> io::Result<()>{
     }
     // success value
     Ok(())
+}
+
+struct BidiMap<A, B> {
+    left_to_right: HashMap<Rc<A>, Rc<B>>,
+    right_to_left: HashMap<Rc<B>, Rc<A>>,
+}
+
+impl<A, B> BidiMap<A, B>
+    where
+        A: Eq + Hash,
+        B: Eq + Hash,
+{
+    fn new() -> Self {
+        BidiMap {
+            left_to_right: HashMap::new(),
+            right_to_left: HashMap::new(),
+        }
+    }
+
+    fn put(&mut self, a: A, b: B) {
+        let a = Rc::new(a);
+        let b = Rc::new(b);
+        self.left_to_right.insert(a.clone(), b.clone());
+        self.right_to_left.insert(b, a);
+    }
+
+    fn get(&self, a: &A) -> Option<&B> {
+        self.left_to_right.get(a).map(Deref::deref)
+    }
+
+    fn get_reverse(&self, b: &B) -> Option<&A> {
+        self.right_to_left.get(b).map(Deref::deref)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct ZipEntry {
+    zip: u16,
+    place: String,
+    // municipality: String,
+    // area: String
 }
 
 // Handle access stream
