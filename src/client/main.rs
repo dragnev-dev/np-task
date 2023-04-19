e35c7eef72367321dd76270cd4cd2300a3d215a0
Dragomir Dragnev <60980007+drago333@users.noreply.github.com>
2023-04-19 13:52:33 +0300

    Refactor client

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,28 +1,35 @@
-use std::str;
+use std::io::{BufRead, BufReader, Write};
 use std::net::TcpStream;
-use std::io::{self,prelude::*,BufReader,Write};
 
-fn main() -> io::Result<( )>{
-    // connect
-    // Struct used to start requests to the server.
-    // Check TcpStream Connection to the server
-    let mut stream = TcpStream::connect("0.0.0.0:8080")?;
-    for _ in 0..1000 {
-        // Allow sender to enter message input
-        let mut input = String::new();
-        // First access the input message and read it
-        io::stdin().read_line(&mut input).expect("Failed to read");
-        // Write the message so that the receiver can access it
-        stream.write(input.as_bytes()).expect("failed to write");
-        // Add buffering so that the receiver can read messages from the stream
-        let mut reader =BufReader::new(&stream);
-        // Check if this input message values are u8
-        let mut buffer: Vec<u8> = Vec::new();
-        // Read input information
-        reader.read_until(b'\n',&mut buffer)?;
+fn main() {
+    // Connect to the server
+    if let Ok(mut stream) = TcpStream::connect("127.0.0.1:46420") {
+        println!("Connected to the server");
+        println!("Enter a request (or 'quit' to exit):");
 
-        println!("read from server:{}",str::from_utf8(&buffer).unwrap());
-        println!("");
+        loop {
+            // Read input from the user
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).unwrap();
+            let key = input.trim();
+
+            if key == "quit" {
+                // Exit loop and close connection
+                break;
+            }
+
+            // Send request to the server
+            stream.write(key.as_bytes()).unwrap();
+            stream.write(b"\n").unwrap();
+
+            // Read response from the server
+            let mut response = String::new();
+            let mut reader = BufReader::new(&stream);
+            reader.read_line(&mut response).unwrap();
+            println!("{}", response);
+        }
+    } else {
+        println!("Failed to connect to server.");
     }
     Ok(())
 }
